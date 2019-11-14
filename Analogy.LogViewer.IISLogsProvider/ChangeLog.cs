fca77cd7effb066d47cf3ba15014848fde680991
Lior Banai <liorbanai@gmail.com>
2019-11-14 19:15:15 +0200

    iniital commit iis logs

---
+++ Analogy.LogViewer.IISLogsProvider/ChangeLog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.Interfaces;
+
+namespace Analogy.LogViewer.IISLogsProvider
+{
+    public static class ChangeLog
+    {
+        public static IEnumerable<AnalogyChangeLog> GetChangeLog()
+        {
+            yield return new AnalogyChangeLog("IIS Data Provider: Initial release", AnalogChangeLogType.Improvement, "Lior Banai", new DateTime(2019, 11, 14));
+
+        }
+    }
+}
---
+++ Analogy.LogViewer.IISLogsProvider/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analogy.LogViewer.IISLogsProvider
+{
+    public static class Utils
+    {
+        public static string GetFileNameAsDataSource(string fileName)
+        {
+            string file = Path.GetFileName(fileName);
+            return fileName.Equals(file) ? fileName : $"{file} ({fileName})";
+
+        }
+    }
+
+}
