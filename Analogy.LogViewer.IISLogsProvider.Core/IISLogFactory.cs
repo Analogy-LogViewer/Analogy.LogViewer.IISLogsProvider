fe185a4599eee3399d42f5679f33aab1b61bb1ba
Lior Banai <liorbanai@gmail.com>
2019-12-14 12:42:35 +0200

    fix #2, fix #3

---
+++ Analogy.LogViewer.IISLogsProvider.Core/IISLogFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.Interfaces;
+using Analogy.Interfaces.Factories;
+
+namespace Analogy.LogViewer.IISLogsProvider
+{
+    public class IISLogFactory : IAnalogyFactory
+    {
+        public static Guid AnalogyNLogGuid { get; } = new Guid("AD7187B1-BD09-4BFE-A28B-3FD3DE879009");
+        public Guid FactoryID { get; } = AnalogyNLogGuid;
+        public string Title { get; } = "IIS Parser";
+        public IAnalogyDataProvidersFactory DataProviders { get; }
+        public IAnalogyCustomActionsFactory Actions { get; }
+        public IEnumerable<IAnalogyChangeLog> ChangeLog => IISLogsProvider.ChangeLog.GetChangeLog();
+        public IEnumerable<string> Contributors { get; } = new List<string> { "Lior Banai" };
+        public string About { get; } = "Analogy IIS Parser";
+
+        public IISLogFactory()
+        {
+            DataProviders = new IISLogsDataProviderFactory();
+            Actions = new AnalogyIISLogCustomActionFactory();
+        }
+
+
+    }
+
+
+    public class AnalogyIISLogCustomActionFactory : IAnalogyCustomActionsFactory
+    {
+        public string Title { get; } = "Analogy IIS Built-In tools";
+        public IEnumerable<IAnalogyCustomAction> Items { get; }
+
+        public AnalogyIISLogCustomActionFactory()
+        {
+            Items = new List<IAnalogyCustomAction>(0);
+        }
+    }
+}
