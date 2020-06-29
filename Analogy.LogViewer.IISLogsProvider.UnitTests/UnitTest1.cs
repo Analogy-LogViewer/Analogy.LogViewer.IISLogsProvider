058f08f5365fa87ce869dbf5c4113a8c0a9f3383
Lior Banai <liorbanai@gmail.com>
2020-06-29 12:19:55 +0300

    update unit tests

--- Analogy.LogViewer.IISLogsProvider.UnitTests/UnitTest1.cs
+++ Analogy.LogViewer.IISLogsProvider.UnitTests/UnitTest1.cs
@@ -1,36 +1,34 @@
-﻿using System;
+﻿using Analogy.Interfaces;
+using Analogy.Interfaces.DataTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Analogy.DataProviders.Extensions;
-using Analogy.Interfaces;
-using Analogy.Interfaces.DataTypes;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Analogy.LogViewer.IISLogsProvider.UnitTests
 {
     [TestClass]
-    public class UnitTest1:ILogMessageCreatedHandler
+    public class UnitTest1 : ILogMessageCreatedHandler
     {
 
         public bool ForceNoFileCaching { get; set; } = true;
         public bool DoNotAddToRecentHistory { get; set; } = true;
         private LogParserSettings LogParserSettings { get; set; }
         private CancellationTokenSource CancellationTokenSource { get; set; }
-        private string filename = @"d:\u_ex190909.log";
+        private string filename = "u_ex_Test.log";
         private List<AnalogyLogMessage> messages;
         [TestMethod]
         public async Task TestMethod1()
         {
-            CancellationTokenSource=new CancellationTokenSource();
-            messages=new  List<AnalogyLogMessage>();
+            CancellationTokenSource = new CancellationTokenSource();
+            messages = new List<AnalogyLogMessage>();
             LogParserSettings = new LogParserSettings();
             LogParserSettings.Splitter = " ";
             LogParserSettings.IsConfigured = true;
             LogParserSettings.SupportedFilesExtensions = new List<string> { "u_ex*.log" };
             IISFileParser p = new IISFileParser(LogParserSettings);
 
-            await p.Process(filename, CancellationTokenSource.Token,this);
+            await p.Process(filename, CancellationTokenSource.Token, this);
         }
 
         public void AppendMessage(AnalogyLogMessage message, string dataSource)
